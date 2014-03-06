e54630efaf65c00b6b9340e2ae687f0a0c496a52
nanakiksc <polcusco@gmail.com>
2014-03-06 21:44:41 +0100

    Faster if query is sorted and works with BED.

--- ting.py
+++ ting.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-# TING:
+# TING: TING Is Not GenomicRanges
+# ((Tree|Thousands) of INtervals based on GenomicRanges)
+# Generate an interval tree from a BED file containing intervals.
 # The module contains a Node class with methods for adding nodes and querying
 # intervals, and functions for building and traversing the tree of nodes.
 # 
-# Generate an interval tree from an input file containing intervals:
-# Eeach line in the input file must contain just one interval.
-# Each line/interval must follow this format: Chromosome, Start, End,
-# Information [and any optional extra Information columns at the end], separated
-# by whitespace (/[\s\t]+/).
-# Example:
-# Chromosome    Start   End     Info                    [Extra Info]
-#                               (e.g. chromatin color)  (e.g. mappability value)
-# chr1          456675  456994  red                     172 
-# Information in the Extra Info column/s is treated as a single column and must
+# Input files have to be in BED format:
+# http://genome.ucsc.edu/FAQ/FAQformat#format1
+# http://www.ensembl.org/info/website/upload/bed.html
+# Information in the optional fields is treated as a single column and must
 # be explicitly unpacked if needed.
 
 import sys
@@ -29,11 +25,13 @@ class Node():
     Create Node instances that store the center position of the node, a list of
     centered intervals overlapping this center, and up to two children nodes.
     """
+
     def __init__(self, center):
         # If center has not a proper value, implicitly raise a ValueError.
         self.center = int(center)
         self.centered = []
         self.children = [None, None]
+        self.is_checked = False
 
     def __repr__(self):
         return 'Node at %d. Overlaps with %d intervals. %d children nodes' \
@@ -47,39 +45,86 @@ class Node():
         Hang a new_child Node instance from the current (parent) node.
         Index is 0 or 1 if new_child is left or right child, respectively.
         """
+
         assert self.children[index] is None, 'Child overwriting!'
         self.children[index] = new_child
 
-    def query_point(self, query):
+    def query_point(self, query, is_sorted):
         """
         Generator function. Yield a list of intervals (iv) in the tree that
         contain the query point.
         """
+
         yield [iv for iv in self.centered if iv[0] <= query <= iv[1]]
 
-        if query < self.center and self.children[0]:
+        if query < self.center \
+        and self.children[0] \
+        and not self.children[0].is_checked:
+ 
+            is_empty = True
             for found in self.children[0].query_point(query):
+                if found:
+                    is_empty = False
                 yield found
 
-        elif self.center < query and self.children[1]:
+            if is_empty and is_sorted:
+                self.children[0].is_checked = True
+
+        elif self.center < query \
+        and self.children[1] \
+        and not self.children[1].is_checked:
+
+            is_empty = True
             for found in self.children[1].query_point(query):
+                if found:
+                    is_empty = False
                 yield found
 
-    def query_interval(self, query):
+            if is_empty and is_sorted:
+                self.children[1].is_checked = True
+
+    def query_interval(self, query, is_sorted):
         """
         Generator funtion. Yield a list of intervals (iv) in the tree that
         overlap, at least partially, with the query interval.
         """
+
         yield [iv for iv in self.centered if not (iv[1] < query[0] \
                                                or query[1] < iv[0])]
 
-        if query[0] < self.center and self.children[0]:
+        if query[0] < self.center \
+        and self.children[0] \
+        and not self.children[0].is_checked:
+
+            is_empty = True
             for found in self.children[0].query_interval(query):
+                if found:
+                    is_empty = False
                 yield found
 
-        if self.center < query[1] and self.children[1]:
+            if is_empty and is_sorted:
+                self.children[0].is_checked = True
+
+        if self.center < query[1] \
+        and self.children[1] \
+        and not self.children[1].is_checked:
+
+            is_empty = True
             for found in self.children[1].query_interval(query):
+                if found:
+                    is_empty = False
                 yield found
+
+            if is_empty and is_sorted:
+                self.children[1].is_checked = True
+
+
+class InputFormatError(Exception):
+    """
+    Raise this exception if input format is not BED.
+    """
+
+    pass
 
 def set_recursion_limit():
     """
@@ -87,6 +132,7 @@ def set_recursion_limit():
     specified or is set at a value < 1000, change it to the specified value
     otherwise.
     """
+
     if MAX_RECURSION_DEPTH and MAX_RECURSION_DEPTH > 1000:
         sys.setrecursionlimit(MAX_RECURSION_DEPTH)
 
@@ -94,16 +140,18 @@ def build_tree(intervals):
     """
     Create a Node instance and initialize its centered list, which contains all
     the intervals that overlap with the center position of the node. Recursively
-    create children nodes containing the remainig intervals. 
+    create children nodes with the remainig intervals. 
     """
+
     set_recursion_limit()
 
-    # Centers have been previously sorted for calculating the median.
-    # s: start, e: end, c: color, i: info.
+    # Centers are sorted to find the median (center).
+    intervals.sort()
+    # s: start, e: end, i: info.
     try:
-        centers = [(s+e)/2.0 for (s,e,c,i) in intervals]
+        centers = [(s+e)/2.0 for (s,e,i) in intervals]
     except ValueError:
-        centers = [(s+e)/2.0 for (s,e,c) in intervals]
+        centers = [(s+e)/2.0 for (s,e) in intervals]
     center = centers[len(centers)//2]
                 
     centered = [i for i in intervals if i[0] <= center <= i[1]]
@@ -124,18 +172,25 @@ def create_trees_dict(intervals_file):
     Create a dictionary in which each key is a chromosome and its value is an
     interval tree containing all the intervals in that chromosome.
     """
+
     # Temp dictionary that holds a list of all the intervals in each chromosme.
     chromosomes = defaultdict(list)
     with open(intervals_file) as fin:
-        sline = (line.rstrip().split(None,4) for line in fin)
-        for inter in sline:
-            if len(inter) == 5:
-                interval = (int(inter[1]),int(inter[2]),inter[3],inter[4])
-            elif len(inter) == 4:
-                interval = (int(inter[1]),int(inter[2]),inter[3])
-            else:
-                raise ValueError('Tree file must have the following format:\n\
-                                 chromosome\tstart\tend\tinfo\t[other_info]')
+        slines = (line.rstrip().split(None,3) for line in fin)
+        for inter in slines:
+            try:
+                interval = [int(inter[1]),int(inter[2])]
+            
+            # Check that the input file is in BED format.
+            except IndexError:
+                raise InputFormatError('Tree file must be in BED format.')
+            except ValueError:
+                raise InputFormatError('Tree file must be in BED format.')
+
+            if len(inter) > 3:
+                interval.append(inter[3])
+                interval = tuple(interval)
+            
             chromosomes[inter[0]].append(interval)
 
     # Final dictionary with one tree per chromosome.
@@ -148,54 +203,79 @@ def create_trees_dict(intervals_file):
  
     return trees
 
-def DFS_interval_query(trees, query):
+def DFS_interval_query(trees, query, is_sorted):
     """
     Generator funtion. Perform a depth-first search of the query interval on the
     interval tree and yield the tree intervals that overlap with the query. 
-    query must be a 3-tuple like (chromosome, start, end).
+    query must be a 3-tuple like (chromosome, start, end) for intervals and a
+    2-tuple like (chromosome, position) for points.
     """
+
     set_recursion_limit()
 
-    if len(query) == 2:
+    if len(query) == 3:
         chrom, start, end = query
         position = (int(start), int(end))
         is_interval = True
-    elif len(query) == 1:
-        position = query
+    elif len(query) == 2:
+        chrom, position = query
         is_interval = False
     else:
-        raise ValueError('Bad query formatting:\n' +
-            'Use a 3-tuple (Chromosome,Start,End) for querying intervals or\n' +
-            'a 2-tuple (Chromosome,Position) for querying points.')
+        raise InputFormatError('Query file must be in BED format.')
 
     total_found = []
     for chromosome in trees:
         if chrom != chromosome:
             continue
-        for found in trees[chromosome].query_interval(position, is_interval):
-            total_found.extend(found)
+        if is_interval:
+            for found in trees[chromosome].query_interval(position, is_sorted):
+                total_found.extend(found)
+        else:
+            for found in trees[chromosome].query_point(position, is_sorted):
+                total_found.extend(found)
     
     if not total_found:
         return
     for hit in total_found:
-        if len(hit) == 5:
-            yield (chrom, hit[0], hit[1], hit[2], hit[3])
-        elif len(hit) == 4:
-            yield (chrom, hit[0], hit[1], hit[2])
+        yield tuple([chrom] + [field for field in hit])
+
+def query_from_main(trees, interval, is_sorted):
+    try:
+        query = [sl[0], int(sl[1])]
+    except IndexError:
+        raise InputFormatError('Query file must be in BED format.')
+    except ValueError:
+        raise InputFormatError('Query file must be in BED format.')
     
+    if len(query) > 2:
+        try:
+            query.append(int(sl[2]))
+        except ValueError:
+            raise InputFormatError(
+                'Query file must be in BED format.'
+                )
+        
+    query = tuple(query)
+    for overlap in DFS_interval_query(trees, query, is_sorted):
+        print overlap
+
+
 if __name__ == '__main__':
     try:
         trees = create_trees_dict(sys.argv[1])
         query_file = sys.argv[2]
+        if sys.argv[3] == '--sorted':
+            query_is_sorted = True
+        elif sys.argv[3] == '--unsorted':
+            query_is_sorted = False
+        else:
+            raise IndexError # Jump to the bottom line.
     
         with open(query_file) as quf:
-            slines = (line.rstrip().split() for line in quf)
-            queries = ((sl[0], int(sl[1]), int(sl[2])) for sl in slines)
+            slines = (line.rstrip().split(None,3) for line in quf)
+            for sl in slines:
+                query_from_main(trees, sl, query_is_sorted)
 
-            for query in queries:
-                for overlap in  DFS_interval_query(trees, query):
-                    print overlap
-    
     except IndexError:
         print ('Please use the following format:\n' +
-               'ting.py <tree_file> <query_file>')
+               'ting.py <tree_file> <query_file> --(sorted|unsorted)')
